32d62f9de43a102951968749b0b305a99cb6b556
Ocholla <eddyphil.ochola@strathmore.edu>
2024-08-07 19:48:59 +0300

    Login and Signup functionality added but problem with recycler view in main xml file

--- app/src/main/java/com/example/chat/Login.kt
+++ app/src/main/java/com/example/chat/Login.kt
@@ -4,29 +4,37 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
 
-    private lateinit var edtName:EditText
+    private lateinit var edtEmail:EditText
     private lateinit var edtPassword:EditText
     private lateinit var btnLogin:Button
     private lateinit var btnRegister: Button
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
+
+        auth = FirebaseAuth.getInstance()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        edtName = findViewById(R.id.edt_name)
+        edtEmail = findViewById(R.id.edt_email)
         edtPassword = findViewById(R.id.edt_password)
         btnLogin = findViewById(R.id.btnLogin)
         btnRegister = findViewById(R.id.btnRegister)
@@ -35,5 +43,27 @@ class Login : AppCompatActivity() {
             var intent = Intent(this, Register::class.java)
             startActivity(intent)
         }
+        btnLogin.setOnClickListener{
+            var email = edtEmail.text.toString()
+            var password = edtPassword.text.toString()
+
+            login(email, password)
+        }
     }
+
+    private fun login(email: String, password: String) {
+//    Logic for logging in user
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Code for jumping to home activity
+                    var intent = Intent(this@Login, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this, "Some error occurred", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
--- app/src/main/java/com/example/chat/Register.kt
+++ app/src/main/java/com/example/chat/Register.kt
@@ -1,20 +1,68 @@
 package com.example.chat
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class Register : AppCompatActivity() {
+
+    private lateinit var edtName: EditText
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnRegister: Button
+    private lateinit var auth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
+
+        supportActionBar?.hide()
+
+        auth = FirebaseAuth.getInstance()
+
+        edtName = findViewById(R.id.edt_name)
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnRegister = findViewById(R.id.btnRegister)
+
+        btnRegister.setOnClickListener {
+            var email = edtEmail.text.toString()
+            var password = edtPassword.text.toString()
+
+            register(email, password)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun register(email: String, password: String){
+        // Logic of creating a user
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Code for jumping to home activity
+
+                    var intent = Intent(this@Register, MainActivity::class.java)
+                    startActivity(intent)
+
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@Register, "Some error occurred", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
