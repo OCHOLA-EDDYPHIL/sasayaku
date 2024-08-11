ab4f0edd9ac5173ac31a208cb0335a4f1834adca
Ocholla <eddyphil.ochola@strathmore.edu>
2024-08-12 02:29:12 +0300

    Splash Screen works

--- app/src/main/java/com/example/chat/SplashActivity.kt
+++ app/src/main/java/com/example/chat/SplashActivity.kt
@@ -2,16 +2,34 @@ package com.example.chat
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // Start MainActivity
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        // Make the splash screen full screen
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val user = FirebaseAuth.getInstance().currentUser
+            if (user != null) {
+                // User is logged in, start MainActivity
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                // User is not logged in, start Login activity
+                startActivity(Intent(this, Login::class.java))
+            }
+            finish()
+        }, 3000) // 3 seconds delay
     }
 }
